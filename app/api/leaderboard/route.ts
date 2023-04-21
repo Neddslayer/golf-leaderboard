e066871c9356f82ab83d1b50f30d6617bbd5b29c
Neddslayer <neddslayer32@gmail.com>
2023-04-21 14:46:02 -0500

    oohhh my gaawwd

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,7 +1,30 @@
+import mongo from "mongodb"
+const dbURI = process.env.DB_URI;
+const MongoClient = require('mongodb').MongoClient;
+
 export async function GET(request: Request) {
   return new Response('no')
 }
 export async function POST(request: Request) {
+  const data = await request.json();
+  try {
+    const client = await MongoClient.connect(dbURI);
+    console.log('Connected successfully to server');
+    const db = client.db("leaderboard");
+    const scoresCollection = db.collection('scores');
+    const { value, username } = data;
+    const result = await scoresCollection.insertOne({ value, username });
+    console.log(`Inserted new score with ID: ${result.insertedId}`);
+    const docs = await scoresCollection.find({}).toArray();
+    console.log('Updated leaderboard:');
+    console.log(docs);
+    client.close();
+    return docs;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+  /*
   const url = 'http://127.0.0.1:8090/api/collections/leaderboards/records';
   
   const data = await request.json();
@@ -21,4 +44,5 @@ export async function POST(request: Request) {
   } else {
     return new Response(response.status.toString());
   }
+  */
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,29 @@ import Image from 'next/image'
 import Link from 'next/link';
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+//const mongo = require('mongodb');
+import mongo from "mongodb"
+const dbURI = process.env.DB_URI;
+const MongoClient = require('mongodb').MongoClient;
+//const dbURI = process.env.DB_URI;
+// 1VriAZ5dg55pBoWh
 const inter = Inter({ subsets: ['latin'] })
 
 async function getLeaderboard() {
-  const res = await fetch('http://127.0.0.1:8090/api/collections/leaderboards/records', { cache: 'no-store' });
-  const data = await res.json();
-  return data?.items as any[];
+  try {
+    const client = await MongoClient.connect(dbURI);
+    console.log('Connected successfully to server');
+    const db = client.db('leaderboard');
+    const scoresCollection = db.collection('score');
+    const docs = await scoresCollection.find({}).toArray();
+    console.log('Found the following records:');
+    console.log(docs);
+    client.close();
+    return docs;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 export default async function Home() {
@@ -17,19 +33,19 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {leaderboard?.sort((a, b) => a.score.value - b.score.value).map((section) => {
+      {leaderboard?.sort((a, b) => a.value - b.value).map((section) => {
         return <LeaderboardSection key={section.id} note={section} />;
       })}
     </main>
   )
 }
 function LeaderboardSection({ note }: any) {
-  const { score } = note || {};
+  const { value, username } = note || {};
 
   return (
     <div className='grid-container' style={{width:'100%', fontSize:'1.5rem', padding: 20}}>
-      <h2 className='grid-item username' style={inter.style}>{score.username}</h2>
-      <h2 className='grid-item value' style={inter.style}>{score.value}</h2>
+      <h2 className='grid-item username' style={inter.style}>{username}</h2>
+      <h2 className='grid-item value' style={inter.style}>{value}</h2>
     </div>
   );
 }
